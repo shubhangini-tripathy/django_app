0d9bad18a711423d23d55b3b0c31804b9594344e
Shubhangini Tripathy <shubhanginitripathy@gmail.com>
2017-12-14 22:28:49 +0530

    Add models

---
+++ tictactoe/gameplay/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Game(models.Model):
+    first_player = models.ForeignKey(
+        User, related_name="games_first_player", on_delete=models.CASCADE)
+    second_palyer = models.ForeignKey(
+        User, related_name="games_second_player", on_delete=models.CASCADE)
+
+    start_time = models.DateTimeField(auto_now_add=True)
+    last_active = models.DateTimeField(auto_now=True)
+
+
+class Move(models.Model):
+    x = models.IntegerField()
+    y = models.IntegerField()
+    comment = models.CharField(max_length=300, blank=True)
+    by_first_player = models.BooleanField()
+
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
